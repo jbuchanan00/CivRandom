25ef800f81a29b46cf0c9e3706c908617c862519
jbuchanan00 <josh.buchanan00@gmail.com>
2020-05-28 12:15:23 -0600

    basic random

---
+++ CivRandom.py
@@ -0,0 +1,14 @@
+import random
+
+civs = ["Teddy Roosevelt", "Saladin", "John Curtain", "Montezuma", "Pedro II",
+        "Wilfrid Laurier", "Qin Shi Huang", "Poundmaker", "Wilhelmina", "Cleopatra",
+        "Victoria", "Eleanor of Aquitaine (England)", "Catherine de Medici",
+        "Eleanor of Aquitaine (French)", "Tamar", "Frederick Barbossa", "Pericles",
+        "Gorgo", "Matthias Corvinus", "Pachacuti", "Ghandi", "Chandragupta",
+        "Gitarja", "Hojo Tokimune", "Jayavarman VII", "Mvemba a Nzinga", "Seodeok",
+        "Alexander", "Mansa Musa", "Kupe", "Lautaro", "Genghis Khan", "Harald Hardrada",
+        "Amanitore", "Suleiman", "Cyrus", "Dido", "Jadwiga", "Trajan", "Peter",
+        "Robert the Bruce", "Tomyris", "Philip II", "Gilgamesh", "Kristina", "Shaka"]
+i = random.randint(1, 42)
+
+print(civs[i])
