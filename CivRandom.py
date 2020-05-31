b09be484b9b8328272fc198e88371b2f4c8e7bc7
jbuchanan00 <josh.buchanan00@gmail.com>
2020-05-31 11:33:20 -0600

    need to add ranking system

--- CivRandom.py
+++ CivRandom.py
@@ -2,7 +2,18 @@ import random
 
 gameType = input("What win type: 1-Dom 2-Culture 3-Science 4-Diplomatic 5-Religion?")
 
-
+if gameType == "1":
+        print(1)
+elif gameType == "2":
+        print(2)
+elif gameType == "3":
+        print(3)
+elif gameType == "4":
+        print(4)
+elif gameType == "5":
+        print(5)
+else:
+        print("You didn't type a valid number")
 
 civs = ["Teddy Roosevelt", "Saladin", "John Curtain", "Montezuma", "Pedro II",
         "Wilfrid Laurier", "Qin Shi Huang", "Poundmaker", "Wilhelmina", "Cleopatra",
